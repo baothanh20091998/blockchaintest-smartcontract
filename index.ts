3064afcb8a3a0ccf9d50351e149d8609ac6cef34
ThanhHLB <ThanhHLB@coin98.finance>
2023-02-12 15:52:03 +0700

    Update send signed transaction on smart contract

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 
-const web3 = new Web3(Web3.givenProvider);
+const web3 = new Web3("https://data-seed-prebsc-1-s1.binance.org:8545/");
 
 const blockchainTestABI: AbiItem[] = [
   {
@@ -22,29 +22,59 @@ const blockchainTestABI: AbiItem[] = [
 const blockchainTestAddress: string =
   "0xc06fdEbA4F7Fa673aCe5E5440ab3d495133EcE7a";
 const testPrivateKey: string =
-  "4bd2e642a117cecf1184841799a95707685b66c7f001d3e4a1a2df132fa15abe";
+  "0xee947e56c328b24cea60f3515ae7db98ac8cea1642f1eea52397032984e9395d";
 
 const main = async () => {
   try {
-    const testAccount = web3.eth.accounts.privateKeyToAccount(testPrivateKey);
-    console.log("account : ", testAccount);
-    console.log("=====================");
-
     const blockchainTestContract = new web3.eth.Contract(
       blockchainTestABI,
       blockchainTestAddress
     );
     console.log("blockchainTestContract: ", blockchainTestContract);
-    console.log("=====================");
 
-    const res = await blockchainTestContract.methods
+    const testAccount = web3.eth.accounts.privateKeyToAccount(testPrivateKey);
+    console.log("account : ", testAccount);
+
+    const gasPrice = await web3.eth.getGasPrice();
+    console.log("Gas price: ", gasPrice);
+
+    const nonce = await web3.eth.getTransactionCount(testAccount.address);
+    console.log("Test account nonce: ", nonce);
+
+    const networkId = await web3.eth.net.getId();
+    console.log("Chain id: ", networkId);
+
+    const gas = await blockchainTestContract.methods
       .set("Hoang Le Bao Thanh")
-      .send({ from: testAccount.address });
+      .estimateGas();
 
-    console.log(
-      "Integrate smart contract and using function set successfully: ",
-      res
+    console.log("Gas: ", gas);
+
+    const data = await blockchainTestContract.methods
+      .set("Hoang Le Bao Thanh")
+      .encodeABI();
+
+    console.log("Data function set in byte code: ", data);
+
+    const signedTransaction = await web3.eth.accounts.signTransaction(
+      {
+        from: testAccount.address,
+        to: blockchainTestAddress,
+        gas,
+        data,
+        gasPrice,
+        nonce,
+        chainId: networkId,
+      },
+      testAccount.privateKey
     );
+    console.log("Signed Transaction: ", signedTransaction);
+
+    const receipt = signedTransaction?.rawTransaction
+      ? await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
+      : null;
+
+    console.log("receipt: ", receipt);
   } catch (error) {
     console.log("Error occur: ", error);
   }
